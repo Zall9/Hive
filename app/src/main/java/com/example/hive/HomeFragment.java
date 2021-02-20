7bf58f5e00b1acc54eafdaf1fb4aa8ade2556566
Zall9 <paul.delifer@yahoo.fr>
2021-02-20 12:59:54 +0100

    Image Display

--- app/src/main/java/com/example/hive/HomeFragment.java
+++ app/src/main/java/com/example/hive/HomeFragment.java
@@ -4,16 +4,50 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment extends Fragment {
+import com.squareup.picasso.Picasso;
 
+public class HomeFragment extends Fragment {
+    String ServerUploadPath ="http://os-vps418.infomaniak.ch:1180/l2_gr_8/img_display.php";
+    ImageView imageView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        //Instantiating the URL class
+        URL url = null;
+        try {
+            url = new URL("http://os-vps418.infomaniak.ch:1180/l2_gr_8/img_display.php");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        //Retrieving the contents of the specified page
+        Scanner sc = null;
+        try {
+            sc = new Scanner(url.openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Instantiating the StringBuffer class to hold the result
+        StringBuffer sb = new StringBuffer();
+        while(sc.hasNext()) {
+            sb.append(sc.next());
+
+            Picasso.get().load(String.valueOf(sb)).into(imageView);
+            //System.out.println(sc.next());
+        }
+        //Retrieving the String from the String Buffer object
+        String result = sb.toString();
+        System.out.println(result);
 
         return inflater.inflate(R.layout.fragment_home, container, false);
+
     }
 }
