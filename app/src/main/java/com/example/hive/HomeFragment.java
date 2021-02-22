abea9f9b5717b5a505efcb8c4032eb238cc04ef3
TheSummer1502 <pajeanromain@gmail.com>
2021-02-22 10:25:35 +0100

    Visualisation de posts dans le fragment home

--- app/src/main/java/com/example/hive/HomeFragment.java
+++ app/src/main/java/com/example/hive/HomeFragment.java
@@ -1,36 +1,164 @@
 package com.example.hive;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Toast;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Scanner;
-import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.squareup.picasso.Picasso;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HomeFragment extends Fragment {
-    String ServerUploadPath ="http://os-vps418.infomaniak.ch:1180/l2_gr_8/img_display.php";
-    private ImageView imageView;
+    List<DataAdapter> ListOfdataAdapter;
+    RecyclerView recyclerView;
+    String HTTP_JSON_URL = "http://os-vps418.infomaniak.ch:1180/l2_gr_8/img_display.php";
+    String Image_Name_JSON = "title";
+    String Image_URL_JSON = "image";
+    // Liste des atributs des Posts
+
+    JsonArrayRequest RequestOfJSonArray ;
+    RequestQueue requestQueue ;
+
+    View view ;
+
+    int RecyclerViewItemPosition ;
+
+    RecyclerView.LayoutManager layoutManagerOfrecyclerView;
+
+    RecyclerView.Adapter recyclerViewadapter;
+
+    ArrayList<String> ImageTitleNameArrayListForClick;
+    ArrayList<String> ImageTitleQuanArrayListForClick;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageTitleNameArrayListForClick = new ArrayList<>();
+        ImageTitleQuanArrayListForClick = new ArrayList<>();
+
+        ListOfdataAdapter = new ArrayList<>();
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview1);
+        recyclerView.setHasFixedSize(true);
+        layoutManagerOfrecyclerView = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManagerOfrecyclerView);
+        JSON_HTTP_CALL();
+
+        // Implementing Click Listener on RecyclerView.
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+
+            GestureDetector gestureDetector = new GestureDetector(getActivity(), new GestureDetector.SimpleOnGestureListener() {
+
+                @Override public boolean onSingleTapUp(MotionEvent motionEvent) {
+
+                    return true;
+                }
+
+            });
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
+
+                view = Recyclerview.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+
+                if(view != null && gestureDetector.onTouchEvent(motionEvent)) {
+
+                    //Getting RecyclerView Clicked Item value.
+                    RecyclerViewItemPosition = Recyclerview.getChildAdapterPosition(view);
+
+                    // Showing RecyclerView Clicked Item value using Toast.
+                    Toast.makeText(getActivity(), ImageTitleNameArrayListForClick.get(RecyclerViewItemPosition), Toast.LENGTH_LONG).show();
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
+        return rootView;
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void JSON_HTTP_CALL(){
 
-        //initialize your view here for use view.findViewById("your view id")
-        imageView = (ImageView) view.findViewById(R.id.imageView);
-        Picasso.get()
-                .load("https://square.github.io/picasso/static/saasdasdmple.png")
-                .into(imageView);
+        RequestOfJSonArray = new JsonArrayRequest(HTTP_JSON_URL,
+
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        ParseJSonResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        requestQueue = Volley.newRequestQueue(getActivity());
+
+        requestQueue.add(RequestOfJSonArray);
+    }
+
+    public void ParseJSonResponse(JSONArray array){
+
+        for(int i = 0; i<array.length(); i++) {
+
+            DataAdapter GetDataAdapter2 = new DataAdapter();
+
+            JSONObject json = null;
+            try {
+
+                json = array.getJSONObject(i);
+
+                GetDataAdapter2.setImageTitle(json.getString(Image_Name_JSON));
+
+                // Adding image title name in array to display on RecyclerView click event.
+                ImageTitleNameArrayListForClick.add(json.getString(Image_Name_JSON));
+                Log.i("URL_IMAGE", json.getString(Image_URL_JSON));
+                GetDataAdapter2.setImageUrl(json.getString(Image_URL_JSON));
+
+            } catch (JSONException e) {
+
+                e.printStackTrace();
+            }
+            ListOfdataAdapter.add(GetDataAdapter2);
+        }
+
+        recyclerViewadapter = new RecyclerViewAdapter(ListOfdataAdapter, getActivity());
+
+        recyclerView.setAdapter(recyclerViewadapter);
     }
 }
+
+
